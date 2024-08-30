a54a0565b7a0eb3f033fc69c6d16c0a54de3afc6
Sador Tilahun <sador.tilahun@elev.ga.ntig.se>
2024-08-30 13:58:07 +0200

    Första

---
+++ 23C_Lucktext/LuckText/LuckText/Program.cs
@@ -0,0 +1,26 @@
+﻿string name;
+
+string age;
+
+string country;
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Clear();
+
+Console.WriteLine("Skriv in ditt namn: ");
+name = Console.ReadLine();
+
+Console.WriteLine("Hur gammal är du?: ");
+age = Console.ReadLine(); 
+
+Console.WriteLine("Vilket land du ifrån: ");
+country = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+// Console.WriteLine("Hello " + name + "!");
+Console.WriteLine($"Hello {name} från {country}! Du är {age} år gammal.");
+                  
+Console.ReadLine();
+
+
